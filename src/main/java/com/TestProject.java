2c082c8c738dff2a92a46b80b3ec2b474b8eaf39
pramesh01 <pramesh.cs@gmail.com>
2022-11-19 09:58:12 +0530

    my first morning comment at 10am

---
+++ src/main/java/com/TestProject.java
@@ -0,0 +1,10 @@
+package com;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		System.out.println("Hello this is my Test Project on Selenium Project.");
+
+	}
+
+}
---
+++ src/test/java/com/TestingProject.java
@@ -0,0 +1,10 @@
+package com;
+
+public class TestingProject {
+
+	public static void main(String[] args) {
+	   System.out.println("hi,This is Pramesh");
+
+	}
+
+}
